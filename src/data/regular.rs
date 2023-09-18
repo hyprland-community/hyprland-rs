4580068e32a13a29ef03730158e7e8b7b30c14f1
nooney <tomaskolarik1@seznam.cz>
2023-09-18 21:49:26 +0200

    data/regular: fix monitor ids (again)

--- src/data/regular.rs
+++ src/data/regular.rs
@@ -94,7 +94,7 @@ pub enum Transforms {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Monitor {
     /// The monitor id
-    pub id: i64,
+    pub id: i128,
     /// The monitor's name
     pub name: String,
     /// The monitor's description
@@ -217,7 +217,7 @@ pub struct Client {
     #[serde(rename = "fullscreenMode")]
     pub fullscreen_mode: i8,
     /// The monitor id the window is on
-    pub monitor: i64,
+    pub monitor: i128,
     /// The initial window class
     #[serde(rename = "initialClass")]
     pub initial_class: String,
