7a71c85f81b5f5398b40385f9c4b038b631aef5e
Enrico Stemmer <enrico@h3rmt.dev>
2026-05-07 14:00:07 +0200

    feat: exec bind methods

--- src/bind_new.rs
+++ src/bind_new.rs
@@ -1,5 +1,7 @@
 use crate::dispatch_new::{Direction, Dispatch, ToDispatch, WindowIdentifier};
+use crate::instance::Instance;
 use crate::lua::{format_bool_field, format_string_field};
+use crate::{command, default_instance};
 use derive_more::Display;
 use std::fmt;
 use std::fmt::Pointer;
@@ -62,7 +64,7 @@ pub struct Binding<D: ToDispatch> {
     /// The key
     pub key: String,
     /// Dispatcher
-    pub dispatch: D,
+    pub dispatcher: D,
     /// Bind flags
     pub flags: Vec<Flag>,
 }
@@ -77,7 +79,7 @@ impl<D: ToDispatch> fmt::Display for Binding<D> {
         f.write_str(&format!("\"{}\"", bind))?;
         f.write_str("), ")?;
 
-        f.write_str(&self.dispatch.to_string())?;
+        f.write_str(&self.dispatcher.to_string())?;
 
         f.write_str(", { ")?;
 
@@ -89,6 +91,35 @@ impl<D: ToDispatch> fmt::Display for Binding<D> {
     }
 }
 
+impl<D: ToDispatch> Binding<D> {
+    /// Binds a keybinding
+    pub fn bind(&self) -> crate::Result<()> {
+        self.instance_bind(default_instance()?)
+    }
+    /// Binds a keybinding
+    pub fn instance_bind(&self, instance: &Instance) -> crate::Result<()> {
+        let lua = self.to_string();
+        instance.write_to_socket(command!(Empty, "eval {}", lua))?;
+        Ok(())
+    }
+
+    /// Binds a keybinding (async)
+    #[cfg(any(feature = "async-lite", feature = "tokio"))]
+    pub async fn bind_async(&self) -> crate::Result<()> {
+        self.instance_bind_async(default_instance()?).await
+    }
+
+    /// Binds a keybinding (async)
+    #[cfg(any(feature = "async-lite", feature = "tokio"))]
+    pub async fn instance_bind_async(&self, instance: &Instance) -> crate::Result<()> {
+        let lua = self.to_string();
+        instance
+            .write_to_socket_async(command!(Empty, "eval {}", lua))
+            .await?;
+        Ok(())
+    }
+}
+
 #[test]
 fn test_key_bindinds() {
     let binds = vec![
@@ -96,7 +127,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![],
                 key: "TAB".to_string(),
-                dispatch: Dispatch::FocusDirection(Direction::Up),
+                dispatcher: Dispatch::FocusDirection(Direction::Up),
                 flags: vec![],
             },
             r#"hl.bind("TAB"), hl.dsp.focus({ direction = "u", }), { })"#,
@@ -105,7 +136,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Super, Mod::Shift],
                 key: "1".to_string(),
-                dispatch: Dispatch::ExecCmd("ls -la".into(), None),
+                dispatcher: Dispatch::ExecCmd("ls -la".into(), None),
                 flags: vec![Flag::Description(r#"move"a""#.into()), Flag::AutoConsuming],
             },
             r#"hl.bind("SUPER + SHIFT + 1"), hl.dsp.exec_cmd("ls -la"), { description = "move\"a\"", auto_consuming = true, })"#,
@@ -114,7 +145,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::FocusLast,
+                dispatcher: Dispatch::FocusLast,
                 flags: vec![],
             },
             r#"hl.bind("ALT + tab"), hl.dsp.focus({ last }), { })"#,
@@ -123,7 +154,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::Exit,
+                dispatcher: Dispatch::Exit,
                 flags: vec![],
             },
             r#"hl.bind("ALT + tab"), hl.dsp.exit(), { })"#,
@@ -132,7 +163,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::SubMap("submap".into()),
+                dispatcher: Dispatch::SubMap("submap".into()),
                 flags: vec![],
             },
             r#"hl.bind("ALT + tab"), hl.dsp.submap("submap"), { })"#,
@@ -141,7 +172,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::Pass(None),
+                dispatcher: Dispatch::Pass(None),
                 flags: vec![],
             },
             r#"hl.bind("ALT + tab"), hl.dsp.pass({ }), { })"#,
@@ -150,7 +181,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::Pass(Some(WindowIdentifier::InitialTitleRegularExpression(
+                dispatcher: Dispatch::Pass(Some(WindowIdentifier::InitialTitleRegularExpression(
                     "Proton P.*\\".to_string(),
                 ))),
                 flags: vec![],
@@ -161,7 +192,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::Event("test".into()),
+                dispatcher: Dispatch::Event("test".into()),
                 flags: vec![],
             },
             r#"hl.bind("ALT + tab"), hl.dsp.event("test"), { })"#,
@@ -170,7 +201,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::ForceIdle(232323),
+                dispatcher: Dispatch::ForceIdle(232323),
                 flags: vec![],
             },
             r#"hl.bind("ALT + tab"), hl.dsp.force_idle(232323), { })"#,
@@ -179,7 +210,7 @@ fn test_key_bindinds() {
             Binding {
                 mods: vec![Mod::Alt],
                 key: "tab".to_string(),
-                dispatch: Dispatch::NoOp(),
+                dispatcher: Dispatch::NoOp(),
                 flags: vec![],
             },
             r#"hl.bind("ALT + tab"), hl.dsp.no_op(), { })"#,
--- src/lua.rs
+++ src/lua.rs
@@ -1,7 +1,21 @@
 use std::fmt::Display;
 
 pub fn format_string<T: Display>(value: &T) -> String {
-    format!("\"{}\"", value.to_string().escape_default())
+    // return format!("\"{}\"", value.to_string().escape_default());
+    let mut f = String::with_capacity(value.to_string().len() + 2);
+    f.push('"');
+    for ch in value.to_string().chars() {
+        match ch {
+            '\\' => f.push_str("\\\\"),
+            '"' => f.push_str("\\\""),
+            '\n' => f.push_str("\\n"),
+            '\r' => f.push_str("\\r"),
+            '\t' => f.push_str("\\t"),
+            c => f.push(c),
+        }
+    }
+    f.push('"');
+    f
 }
 
 pub fn format_string_field<T: Display>(key: &str, value: T) -> String {
@@ -32,4 +46,20 @@ pub fn format_raw_field_opt<T: Display>(key: &str, value: &Option<T>) -> String 
         Some(v) => format_raw_field(key, v),
         None => String::new(),
     }
+}
+
+#[test]
+fn test_format_string() {
+    assert_eq!(format_string(&r#" test "#), r#"" test ""#);
+    assert_eq!(format_string(&r#" "test" "#), r#"" \"test\" ""#);
+    assert_eq!(format_string(&r#" "test\n" "#), r#"" \"test\\n\" ""#);
+    assert_eq!(format_string(&r#" "test\r" "#), r#"" \"test\\r\" ""#);
+    assert_eq!(format_string(&r#" "test\t" "#), r#"" \"test\\t\" ""#);
+    assert_eq!(format_string(&r#" test\\ "#), r#"" test\\\\ ""#);
+    assert_eq!(format_string(&r#" "test\"" "#), r#"" \"test\\\"\" ""#);
+    assert_eq!(
+        format_string(&r#" "test\n\r\t\\" "#),
+        r#"" \"test\\n\\r\\t\\\\\" ""#
+    );
+    assert_eq!(format_string(&r#" test's "#), r#"" test's ""#);
 }
